3771f2a19d76f680e24eadef4fdcb48be2e2a8c0
Joe Petrakovich <jpetrakovich@bankdirectsystems.com>
2023-02-23 22:09:26 -0600

    added sapphire calls

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
     import { ethers } from 'ethers'
+    import * as sapphire from '@oasisprotocol/sapphire-paratime';
+    import Whisper from '$lib/Whisper.json';
     import Shout from '$lib/Shout.json';
     import { onMount } from 'svelte';
 
-    const CONTRACT_ADDRESS="0xea839C7542FB2419B9f71A0bdff6D4fa3dbca792";
+    const SHOUT_CONTRACT_ADDRESS="0xea839C7542FB2419B9f71A0bdff6D4fa3dbca792";
+    const WHISPER_CONTRACT_ADDRESS="0xebcc16233DE34C1945214C92896eb88182bC8def";
 
     type Message = { from: string; text: string; timestamp: string };
 
@@ -19,7 +22,7 @@
 		const provider = new ethers.BrowserProvider(window.ethereum);
         const signer = await provider.getSigner();
 		const shoutContract = new ethers.Contract(
-			CONTRACT_ADDRESS,
+			SHOUT_CONTRACT_ADDRESS,
 			Shout.abi,
             signer
 		);
@@ -42,7 +45,7 @@
             const provider = new ethers.BrowserProvider(window.ethereum);
             const signer = await provider.getSigner();
             const shoutContract = new ethers.Contract(
-                CONTRACT_ADDRESS,
+                SHOUT_CONTRACT_ADDRESS,
                 Shout.abi,
                 signer
             );
@@ -60,17 +63,68 @@
         }
     }
 
+    async function getSecretMessages() {
+		if (!window.ethereum) {
+			return;
+		}
+
+        window.ethereum = sapphire.wrap(window.ethereum);
+
+		const whisperContract = new ethers.Contract(
+			WHISPER_CONTRACT_ADDRESS,
+			Whisper.abi,
+            await new ethers.BrowserProvider(window.ethereum).getSigner()
+		);
+
+        let receivedMessages = await whisperContract.getMessages();
+
+         const normalizeMessage = (message: any) => ({
+             from: message.from,
+             text: message.text,
+             timestamp: message.timestamp,
+         });
+
+         messages = receivedMessages
+             .map(normalizeMessage)
+             .sort((a: any, b: any) => b.timestamp - a.timestamp);
+    }
+
+    async function sendSecretMessage() {   
+        try {
+            window.ethereum = sapphire.wrap(window.ethereum);
+
+            const whisperContract = new ethers.Contract(
+                WHISPER_CONTRACT_ADDRESS,
+                Whisper.abi,
+                await new ethers.BrowserProvider(window.ethereum).getSigner()
+            );
+
+            const transaction = await whisperContract.sendMessage(address, message, {
+                gasLimit: 400000,
+            });
+            await transaction.wait();
+
+            address = '';
+            message = '';
+        
+        } catch (error) {
+            alert('Error while sending message: ' + error);
+        }
+    }
+
     onMount(() => {
-        getMessages();
+        //getMessages();
+        getSecretMessages();
     });
 </script>
 
 <div>
-    <h1>Shout & Whisper</h1>
+    <h1>Whisper & Shout</h1>
 
     <input bind:value={address} type="text" placeholder="Address" />
     <textarea bind:value={message} rows="5" cols="33" placeholder="Write something..." />
-    <button on:click={sendMessage}>Send</button>
+    <button on:click={sendMessage}>Shout</button>
+    <button on:click={sendSecretMessage}>Whisper</button>
 
     <h2>Messages</h2>
         {#if messages.length} 
